fd3e52cbae0869a673eda826e2d765c18313f848
Thibault Saunier <tsaunier@igalia.com>
2019-02-08 16:44:39 -0300

    clip: Add a method to get the priority of the layer it is in
    Just an helper method to get the 'priority of a the clip'

--- ges/ges-internal.h
+++ ges/ges-internal.h
@@ -353,7 +353,6 @@ G_GNUC_INTERNAL void _ges_container_set_priority_offset   (GESContainer * contai
  ****************************************************/
 G_GNUC_INTERNAL void              ges_clip_set_layer              (GESClip *clip, GESLayer  *layer);
 G_GNUC_INTERNAL gboolean          ges_clip_is_moving_from_layer   (GESClip *clip);
-G_GNUC_INTERNAL guint32           ges_clip_get_layer_priority     (GESClip *clip);
 G_GNUC_INTERNAL void              ges_clip_set_moving_from_layer  (GESClip *clip, gboolean is_moving);
 G_GNUC_INTERNAL GESTrackElement*  ges_clip_create_track_element   (GESClip *clip, GESTrackType type);
 G_GNUC_INTERNAL GList*            ges_clip_create_track_elements  (GESClip *clip, GESTrackType type);
